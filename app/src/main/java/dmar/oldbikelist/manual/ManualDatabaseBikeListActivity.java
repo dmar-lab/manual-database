5d0b622dff6cac2785450c5e2acd2e2d1a0fe37d
Dariusz Marszałek <darekm770@gmail.com>
2023-02-10 19:00:13 +0100

    Revert "update build.gradle for Kotlin"
    This reverts commit 95d0713286d58202f477aba9ff77a7b5c2f1c398.

--- app/src/main/java/dmar/oldbikelist/manual/ManualDatabaseBikeListActivity.java
+++ app/src/main/java/dmar/oldbikelist/manual/ManualDatabaseBikeListActivity.java
@@ -1,11 +1,10 @@
 package dmar.oldbikelist.manual;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
